601c2b27e3f190c7660f5d7327885d79e23b0300
lauralen <laura.l@8devices.com>
2020-12-20 12:22:42 +0200

    Add map layers control

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, LayersControl } from "react-leaflet";
 import openWeatherKey from "../utils/openWeatherKey";
 import style from "./Map.module.scss";
 
 function Map({ data }) {
   const { lon, lat } = data.coord;
   const position = [lat, lon];
+
+  const layers = [
+    { value: "clouds", name: "Clouds" },
+    { value: "precipitation", name: "Precipitation" },
+    { value: "pressure", name: "Sea level pressure" },
+    { value: "wind", name: "Wind speed" },
+    { value: "temp", name: "Temperature" }
+  ];
 
   return (
     <div className={style.mapWrapper}>
@@ -19,10 +27,19 @@ function Map({ data }) {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <TileLayer
-          attribution='Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>'
-          url={`http://{s}.tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${openWeatherKey}`}
-        />
+        <LayersControl>
+          {layers.map(option => {
+            const { value, name } = option;
+            return (
+              <LayersControl.BaseLayer key={value} name={name}>
+                <TileLayer
+                  attribution='Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>'
+                  url={`http://{s}.tile.openweathermap.org/map/${value}_new/{z}/{x}/{y}.png?appid=${openWeatherKey}`}
+                />
+              </LayersControl.BaseLayer>
+            );
+          })}
+        </LayersControl>
         <Marker position={position} />
       </MapContainer>
     </div>
