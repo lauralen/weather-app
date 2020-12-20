98c1316b69e24e0a5cc3afa2404162d86f94b7fd
lauralen <laura.l@8devices.com>
2020-12-20 11:48:40 +0200

    Add clouds layer to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import openWeatherKey from "../utils/openWeatherKey";
 import style from "./Map.module.scss";
 
 function Map({ data }) {
@@ -10,13 +11,17 @@ function Map({ data }) {
     <div className={style.mapWrapper}>
       <MapContainer
         center={position}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={false}
         style={{ height: "50vh", width: "100%" }}
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <TileLayer
+          attribution='Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>'
+          url={`http://{s}.tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${openWeatherKey}`}
         />
         <Marker position={position} />
       </MapContainer>
