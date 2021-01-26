6fb15e450156912534cf7c787dbfbeee740e72db
lauralen <laurallenkutyte@gmail.com>
2021-01-26 20:23:52 +0200

    Update weather card style

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Button from "./Button";
-import Map from "./Map";
 import style from "./WeatherCard.module.scss";
 
+import Button from "./Button";
+import Map from "./Map";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faStar,
+  faTint,
+  faWind,
+  faCloud
+} from "@fortawesome/free-solid-svg-icons";
+
 const WeatherCard = ({ data, favorites, setFavorites }) => {
-  const { name, sys, main, weather, wind, units, coord } = data;
-  const { temp, feels_like, humidity } = main;
+  const { name, sys, main, weather, wind, units, coord, clouds } = data;
+  const { temp_max, temp_min, humidity } = main;
   const { lon, lat } = coord;
   const { country } = sys;
   const { description, icon } = weather[0];
@@ -19,55 +28,58 @@ const WeatherCard = ({ data, favorites, setFavorites }) => {
 
   return (
     <div className={style.container}>
-      <div className={style.header}>
+      <div className={style.main}>
+        <div className={style.favoriteWrapper}>
+          {isFavorite || favorites.length > 20 ? null : (
+            <Button
+              type="primary"
+              onClick={() => {
+                setFavorites([...favorites, { name, country }]);
+              }}
+            >
+              <FontAwesomeIcon icon={faStar} />
+            </Button>
+          )}
+        </div>
+
         <h2>
           {name}, {country}
         </h2>
-        {isFavorite || favorites.length > 20 ? null : (
-          <Button
-            type="primary"
-            onClick={() => {
-              setFavorites([...favorites, { name, country }]);
-            }}
-          >
-            + Add to favorites
-          </Button>
-        )}
+
+        <div>
+          <img
+            className={style.icon}
+            src={`https://openweathermap.org/img/w/${icon}.png`}
+            alt="Weather icon"
+          />
+          <p className={style.description}>{description}</p>
+          <h3>
+            {Math.round(temp_max)}
+            {unit.temperature} / {Math.round(temp_min)}
+            {unit.temperature}
+          </h3>
+        </div>
+
+        <ul className={style.list}>
+          <li>
+            <FontAwesomeIcon icon={faTint} />
+            <span>Humidity</span>
+            <span>{humidity}%</span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faWind} />
+            <span>Wind speed</span>
+            <span>
+              {Math.round(wind.speed)} {unit.speed}
+            </span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCloud} />
+            <span>Cloudiness</span>
+            <span>{clouds.all} %</span>
+          </li>
+        </ul>
       </div>
-
-      <img
-        className={style.icon}
-        src={`https://openweathermap.org/img/w/${icon}.png`}
-        alt="Weather icon"
-      />
-      <p className={style.description}>{description}</p>
-
-      <ul className={style.list}>
-        <li>
-          <span>Temperature</span>
-          <span>
-            {Math.round(temp)}
-            {unit.temperature}
-          </span>
-        </li>
-        <li>
-          <span>Feels like</span>
-          <span>
-            {Math.round(feels_like)}
-            {unit.temperature}
-          </span>
-        </li>
-        <li>
-          <span>Humidity</span>
-          <span>{humidity}%</span>
-        </li>
-        <li>
-          <span>Wind speed</span>
-          <span>
-            {Math.round(wind.speed)} {unit.speed}
-          </span>
-        </li>
-      </ul>
 
       <Map position={[lat, lon]} />
     </div>
